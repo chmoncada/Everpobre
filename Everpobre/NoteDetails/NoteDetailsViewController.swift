2e3d0a9b4c67092d7961ad05ae5b259772fd39a7
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-08 19:51:27 -0300

    Initial NoteDetailsViewController setup

---
+++ Everpobre/NoteDetails/NoteDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NoteDetailsViewController.swift
+//  Everpobre
+//
+//  Created by Charles Moncada on 08/10/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
