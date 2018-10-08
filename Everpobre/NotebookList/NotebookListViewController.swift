1a6aea381ebddfb1385dea55c41b197a10b1f1cf
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-08 17:34:59 -0300

    Initial setup

---
+++ Everpobre/NotebookList/NotebookListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NotebookListViewController.swift
+//  Everpobre
+//
+//  Created by Charles Moncada on 08/10/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import UIKit
+
+class NotebookListViewController: UIViewController {
+
+	
+}
