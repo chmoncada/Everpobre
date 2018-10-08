844329cf697aa6ee0dfdd4be9eead0aec4fa8a58
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-08 17:01:54 -0300

    Initial Commit

---
+++ Everpobre/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Everpobre
+//
+//  Created by Charles Moncada on 08/10/18.
+//  Copyright © 2018 Charles Moncada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
