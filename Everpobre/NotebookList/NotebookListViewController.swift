c79e29ed9cbf280b8a05462495fa49d5739dc8b1
Charles Moncada <charles.moncada@emoshiapps.com>
2018-10-11 16:57:59 -0300

    Añadimos un label total y creamos NSFetchRequest para la suma

--- Everpobre/NotebookList/NotebookListViewController.swift
+++ Everpobre/NotebookList/NotebookListViewController.swift
@@ -12,7 +12,8 @@ import CoreData
 class NotebookListViewController: UIViewController {
 
 	@IBOutlet weak var tableView: UITableView!
-
+	@IBOutlet weak var totalLabel: UILabel!
+	
 	var managedContext: NSManagedObjectContext! // Beware to have a value before presenting the VC
 
 //	var model: [deprecated_Notebook] = [] {
@@ -21,14 +22,15 @@ class NotebookListViewController: UIViewController {
 //		}
 //	}
 
-	var dataSource: [NSManagedObject] {
-		do {
-			return try managedContext.fetch(Notebook.fetchRequest())
-		} catch let error as NSError {
-			print(error.localizedDescription)
-			return []
-		}
-	}
+	var dataSource: [NSManagedObject] = []
+//	{
+//		do {
+//			return try managedContext.fetch(Notebook.fetchRequest())
+//		} catch let error as NSError {
+//			print(error.localizedDescription)
+//			return []
+//		}
+//	}
 
 	override func viewDidLoad() {
 		//model = deprecated_Notebook.dummyNotebookModel
@@ -36,6 +38,38 @@ class NotebookListViewController: UIViewController {
 		navigationController?.navigationItem.largeTitleDisplayMode = .always
 		
 		super.viewDidLoad()
+
+		reloadView()
+	}
+
+	private func reloadView() {
+
+		do {
+			dataSource = try managedContext.fetch(Notebook.fetchRequest())
+		} catch let error as NSError {
+			print(error.localizedDescription)
+			dataSource = []
+		}
+
+		populateTotalLabel()
+		tableView.reloadData()
+	}
+
+	private func populateTotalLabel() {
+		let fetchRequest = NSFetchRequest<NSNumber>(entityName: "Notebook")
+		fetchRequest.resultType = .countResultType
+
+		let predicate = NSPredicate(value: true)
+		fetchRequest.predicate = predicate
+
+		do {
+			let countResult = try managedContext.fetch(fetchRequest)
+			let count = countResult.first!.intValue
+			totalLabel.text = "\(count)"
+		} catch let error as NSError {
+			print("Count not fetch: \(error.description)")
+		}
+
 	}
 	
 	@IBAction func addNotebook(_ sender: UIBarButtonItem) {
@@ -57,7 +91,8 @@ class NotebookListViewController: UIViewController {
 				print("TODO Error handling: \(error.debugDescription)")
 			}
 
-			self.tableView.reloadData()
+			//self.tableView.reloadData()
+			self.reloadView()
 		}
 
 		let cancelAction = UIAlertAction(title: "Cancelar", style: .default)
@@ -100,13 +135,13 @@ extension NotebookListViewController: UITableViewDataSource {
 
 		do {
 			try managedContext.save()
-			tableView.deleteRows(at: [indexPath], with: .automatic)
+			//tableView.deleteRows(at: [indexPath], with: .automatic)
 		} catch let error as NSError {
 			print("error: \(error.localizedDescription)")
 		}
 
-		tableView.reloadData()
-
+		//tableView.reloadData()
+		reloadView()
 	}
 }
 
@@ -123,7 +158,7 @@ extension NotebookListViewController: UITableViewDelegate {
 //		show(notesListVC, sender: nil)
 
 		let notebook = dataSource[indexPath.row] as! Notebook
-		let notesListVC = NotesListViewController(notebook: notebook)
+		let notesListVC = NotesListViewController(notebook: notebook, managedContext: managedContext)
 		show(notesListVC, sender: nil)
 	}
 }
